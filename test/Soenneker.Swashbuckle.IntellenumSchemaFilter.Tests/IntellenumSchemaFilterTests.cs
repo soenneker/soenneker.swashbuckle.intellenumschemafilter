5583b1094d29533107647c6ab7f2d839178c7e26
Jake Soenneker <jake@soenneker.com>
2026-05-21 14:50:01 -0500

    swashbuckle updates

--- test/Soenneker.Swashbuckle.IntellenumSchemaFilter.Tests/IntellenumSchemaFilterTests.cs
+++ test/Soenneker.Swashbuckle.IntellenumSchemaFilter.Tests/IntellenumSchemaFilterTests.cs
@@ -1,4 +1,6 @@
+using Microsoft.OpenApi;
 using Soenneker.Tests.HostedUnit;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Soenneker.Swashbuckle.IntellenumSchemaFilter.Tests;
 
@@ -15,5 +17,15 @@ public class IntellenumSchemaFilterTests : HostedUnitTest
     public void Default()
     {
 
+    }
+
+    [Test]
+    public void Apply_should_ignore_schema_references()
+    {
+        var filter = new IntellenumSchemaFilter();
+        var schema = new OpenApiSchemaReference("RequestDataOptions", new OpenApiDocument(), "3.0");
+        var context = new SchemaFilterContext(typeof(string), null!, new SchemaRepository(), null, null);
+
+        filter.Apply(schema, context);
     }
 }
